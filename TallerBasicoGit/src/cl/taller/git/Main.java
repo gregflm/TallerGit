a45069c38be06dcf926b7c2da06c643f3cf3e640
gregflm <llanosgreg@gmail.com>
2014-04-07 17:19:48 -0300

    Update Main.java

--- TallerBasicoGit/src/cl/taller/git/Main.java
+++ TallerBasicoGit/src/cl/taller/git/Main.java
@@ -5,7 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 
 		System.out.println("Segundo Commit");
-		
+				System.out.println("Segundo cacaCommit");
+
 				System.out.println("Sincroniza con eclipse team> remote > fetch from");
 
 		
